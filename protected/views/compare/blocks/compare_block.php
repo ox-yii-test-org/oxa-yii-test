08d0c627932bb202f1832e20ba583545b9406232
pavel.tivunov <pavel.tivunov@oxagile.com>
2014-10-10 10:29:21 +0300

    friendlier compare items view

--- protected/views/compare/blocks/compare_block.php
+++ protected/views/compare/blocks/compare_block.php
@@ -2,23 +2,22 @@
     <?php if ($models):?>
         <?php foreach ($models as $model):?>
             <div class="view" data-id="<?php echo $model->id?>">
-                <b><?php echo CHtml::encode($model->getAttributeLabel('name')); ?>:</b>
-                <?php echo CHtml::encode($model->name); ?>
-                <br />
-
-                <b><?php echo CHtml::encode($model->getAttributeLabel('desc')); ?>:</b>
-                <?php echo CHtml::encode($model->desc); ?>
-                <br />
-
-                <b><?php echo CHtml::encode($model->getAttributeLabel('status')); ?>:</b>
-                <?php echo CHtml::encode(Male::getMaleStatus($model->status)); ?>
-                <br />
-
-                <b><?php echo CHtml::encode($model->getAttributeLabel('image')); ?>:</b>
-                <?php if($model->image) {?>
+                <?php if($model->image) :?>
                     <?php echo CHtml::image(Yii::app()->controller->createUrl($model->tableName() . '/loadImage', array('id' => $model->id)), 'image', array('width' => 200));?>
-                <?php }?>
-                <br />
+                <?php endif;?>
+                <div class="compare-info">
+                    <b><?php echo CHtml::encode($model->getAttributeLabel('name')); ?>:</b>
+                    <?php echo CHtml::encode($model->name); ?>
+                    <br />
+
+                    <b><?php echo CHtml::encode($model->getAttributeLabel('desc')); ?>:</b>
+                    <?php echo CHtml::encode($model->desc); ?>
+                    <br />
+
+                    <b><?php echo CHtml::encode($model->getAttributeLabel('status')); ?>:</b>
+                    <?php echo CHtml::encode(Male::getMaleStatus($model->status)); ?>
+                    <br />
+                </div>
             </div>
         <?php endforeach;?>
     <?php else:?>
